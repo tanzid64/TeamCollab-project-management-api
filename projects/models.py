7113a6816445620f48438162b5add8b07f84c49b
tanzid64 <tanzid@inbox.ru>
2024-06-25 16:52:04 +0600

    Project model created..

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+# Store Project Data
+class Project(models.Model):
+  name = models.CharField(max_length=100)
+  owner = models.ForeignKey(User,related_name="projects", on_delete=models.CASCADE)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
